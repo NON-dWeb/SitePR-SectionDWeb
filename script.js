0034846b824c474dbab1321fe3ba3583d0595e1d
NON-dWeb <noemienino94@gmail.com>
2020-10-29 09:44:24 -0400

    essai pas bon

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-#txt3
-    { font-size:30px;
-    animation-name: slide;
-    animation-duration: 10s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-    -webkit-animation-name: slide;
-    -webkit-animation-duration: 10s;
-    -webkit-animation-timing-function:linear;
-    -webkit-animation-iteration-count: infinite;}
+
   
   
