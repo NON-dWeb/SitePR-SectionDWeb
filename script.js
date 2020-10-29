05e9305b485f03a8370142b95e077727ff7af968
NON-dWeb <noemienino94@gmail.com>
2020-10-29 11:00:21 -0400

    fonctionnementheadertest3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const titre=document.querySelector("header")
-titre.style.position = "absolute";
+header.style.position = "absolute";
 let topPos = 0;
 let dir =-1;
 
   function hautBas () {if (topPos == 300) {dir = 1 }
   else if (topPOS == -50) {dir = -1 }
-     topPos += -2 * dir; titre.style.top= `${topPos}px`;  requestAnimationFrame(hautBas);}
+     topPos += -2 * dir; header.style.top= `${topPos}px`;  requestAnimationFrame(hautBas);}
   requestAnimationFrame(hautBas);
