363652d29a09dce8aca4963c43cfa647130a56e9
NON-dWeb <noemienino94@gmail.com>
2020-10-29 09:43:29 -0400

    essaijava

---
+++ script.js
@@ -0,0 +1,12 @@
+#txt3
+    { font-size:30px;
+    animation-name: slide;
+    animation-duration: 10s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    -webkit-animation-name: slide;
+    -webkit-animation-duration: 10s;
+    -webkit-animation-timing-function:linear;
+    -webkit-animation-iteration-count: infinite;}
+  
+  
