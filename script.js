11aaedba4449edced86bb03037f572697e644989
NON-dWeb <noemienino94@gmail.com>
2020-11-03 15:46:37 -0400

    modif html dweb

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-$(title).ready(function(){
-    function marqueePlay(){
-        $("#home-big-title").animate(
-            {
-            left: $(window).width() - $("#home-big-title").width(),
-            opacity: 0
-            }, 10000, function(){
-                $("#home-big-title").css("left", 0);
-                $("#home-big-title").css("opacity", 1);
-                marqueePlay();
-            }
-        );
-    }
-    marqueePlay();
-}); 
 
